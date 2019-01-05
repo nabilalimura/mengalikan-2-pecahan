a6ea9d0f94701a63822b2464f2c7a2003353185e
NABIL ALIMURA <45660685+nabilalimura@users.noreply.github.com>
2019-01-05 11:06:16 +0700

    Add files via upload

---
+++ mengalikan 2 pecahan.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main () {
+    float hasil;
+
+cout<<"MENGALIKAN 2 PECAHAN "<<endl;
+cout<<"NAMA : NABIL ALIMURA"<<endl;
+cout<<"KELAS : 18B2"<<endl;
+cout<<"NIM : 311810926"<<endl;
+cout<<endl;
+
+cout<<"P1= 3.0 / 4.0"<<endl;
+cout<<"P2= 2.0 /3.0"<<endl;
+
+hasil=(3.0*2.0) / (3.0*4.0);
+
+cout<<"Maka Hasilnya Adalah : "<<hasil;
+return 0;
+}
